1105465343e227158b058c4f275969a696a712c4
nath4ns <nathansousa360@gmail.com>
2021-01-23 23:54:26 -0300

    dados

---
+++ scripts.js
@@ -0,0 +1,37 @@
+/*
+    Prototype
+
+    * Prototype-based language
+    * propotype chain
+    * _proto
+
+*/
+
+/*
+    Tyoe conversion (typecasting) vs Type coersion
+
+    * Alteração de um tipo de dado para outro tipo
+
+    console.log(Number('9') + 5)
+
+*/
+
+/*
+    Manipulando Strings e Números
+
+    . Transformar String em Número e Número em String
+    let string = "123"
+    console.log(Number(string))
+    let number = 321
+    console.log(String(number))
+
+    . Contar quantos caracteres tem uma palavra e quantos digítos tem um número
+    let word = "Paralelepipedo"
+    console.log(word.length)
+    let number = 12345
+    console.log(String(number).length)
+
+*/
+
+
+
