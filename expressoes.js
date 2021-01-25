38d7c1697321cfde6fad19d09f0f79c5e5d3730b
nath4ns <nathansousa360@gmail.com>
2021-01-24 22:27:20 -0300

    operadores

---
+++ expressoes.js
@@ -0,0 +1,27 @@
+/*   Expresões e Operadores
+
+    - Expressions
+    - Operators
+        Binary
+            number + 1
+        Unary
+            ++number
+        Ternary
+            console.log(true ? 'alo' : 'nada')
+*/
+
+/*
+    New
+
+    * left-hand-side expression
+    * Criar um novo objeto
+
+
+    let name = new String('Mayk')
+    name.surName = "Brito"
+    let age = new Number(23)
+    console.log(name.surName, age)
+
+    let date = new Date('2020-12-01')
+    console.log(date.__proto__)
+*/
---
+++ operadores.js
@@ -0,0 +1,55 @@
+/*  
+    Operadores unários
+        typeof
+        delete
+
+    const person = {
+        name: 'Nathan',
+        age: 20,
+    }
+    delete person.age
+
+    console.log(person)
+    //console.log(typeof "Nathan")
+*/
+
+/*
+    Operadores Aritiméticos
+
+    // Multiplicação *
+        console.log(3.2 * 5.5)
+
+    // Divisão /
+        console.log(12 / 2)
+
+    // Soma +
+        console.log(34 + 50)
+
+    // Subtração -
+        console.log(34 - 23)
+
+    // Resto da divisão %
+        let remaider
+        remaider = 11 % 10
+        console.log(remaider)
+    
+    // Incremento ++
+        let increment = 0
+        increment++
+        console.log(increment)
+
+        //Decremento --
+            let decrememt = 0
+            decrememt--
+            console.log(decrememt)
+
+    // Exponencial **
+        console.log(2 ** 2)
+*/
+
+/*
+    Grouping operator ( )
+    
+    let total = (2 + 3) * 5
+    console.log((total)) 
+*/
