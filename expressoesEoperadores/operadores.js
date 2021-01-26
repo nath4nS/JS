543bd5ae2006eb0702f959496e635eb4a3888d3a
nath4ns <nathansousa360@gmail.com>
2021-01-26 15:11:17 -0300

    operadores

---
+++ expressoesEoperadores/operadores.js
@@ -0,0 +1,62 @@
+/*  
+    Operadores unários
+        typeof
+        delete
+
+    const person = {
+        name: 'Nathan',
+        age: 20,
+    }
+    delete person.age
+
+    console.log(person)
+    //console.log(typeof "Nathan")
+*/  
+
+
+/*
+    Grouping operator ( )
+    
+    let total = (2 + 3) * 5
+    console.log((total)) 
+*/
+
+
+/*
+    Operador de String (String Operator)
+
+    // comparison (comparação)
+        console.log( 'a' == 'a')
+
+    // concatenation (concatenação)
+    // Retorna a união de suas Strings 
+        let alpha = 'alpha'
+        alpha += 'bet'
+        console.log(alpha + 345)
+*/
+
+
+/*
+    Type conversion (typecarting) vs Type coersion
+        console.log('9' + 5)
+*/
+
+
+/*
+    Operator precedence
+        Precedência de operadores
+
+    * grouping                        ( )
+        console.log( (2 + 5) * 10 )
+    * negação e incremento            ! ++ --
+    * multiplicação e divisão         * /
+    * adição e subtração              + -
+    * relacional                      < <= > >=c
+        console.log( 3 > 2 > 1  )
+    * igualdade                       == != === !==
+    * AND                             &&
+        console.log( 3 > 1 && 2 > 1 )
+    * OR                              ||
+    * condicional                     ?:
+    * assignment (atribuição)         = += -= *=
+*/
