db501af7b1c171d93845f1f89726933a7f3d50b2
nath4ns <nathansousa360@gmail.com>
2021-02-16 23:02:53 -0300

    exercícios

--- exercicios/arrays.js
+++ exercicios/arrays.js
@@ -55,14 +55,53 @@ for(let category of booksByCategory) {
     console.log(category.books.length)
 }
 
+// contar o número de autores
 function countAuthors() {
     let authors = [];
 
     for(let category of booksByCategory) {
         for(let book of category.books) {
             if(authors.indexOf(book.author) == -1) {
-                authors.push(book.title)
+                authors.push(book.author)
             }
         }
     }
+
+    console.log("Total de autores: ", authors.length)
 }
+
+countAuthors();
+
+// Mostrar livros do autor Augusto Cury
+function booksOfAugustoCury() {
+    let books = [];
+
+    for(let category of booksByCategory) {
+        for(let book of category.books) {
+            if(book.author === 'Augusto Cury') {
+                books.push(book.title)
+            }
+        }
+    }
+
+    console.log("Livros do autor: ", books)
+}
+booksOfAugustoCury()
+
+
+/* Transformar a função acima em uma função que irá receber o nome do autor e
+        devolver os livros desse autor.*/
+function booksOfAuthor(author) {
+    let books = [];
+
+    for(let category of booksByCategory) {
+        for(let book of category.books) {
+            if(book.author === author) {
+                books.push(book.title)
+            }
+        }
+    }
+
+    console.log(`Livros do autor ${author}: ${books.join(", ")}`)
+}
+booksOfAuthor('George S. Clason')
